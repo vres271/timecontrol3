0a0286b46cad3c6b176f24ba76fe0799d8c45873
vres271 <vres271@gmail.com>
2020-08-26 23:36:34 +0400

    Test mode : display values

--- timecontrol3.ino
+++ timecontrol3.ino
@@ -19,15 +19,15 @@ Encoder enc1(CLK, DT, SW);  // для работы c кнопкой
 #define LASER_SENS_GND 9
 #define LASER_SENS_S A7
 
-unsigned long lastLaserOnTime, lastDispRefreshTime, startImpTime, endImpTime, t;
+unsigned long lastLaserOnTime,  startImpTime, endImpTime, t;
 boolean laserState = false;
 unsigned int laserSensLevel, laserSens= 0;
 
 //byte eventCode = 0;
 
 struct State{
-  unsigned int route = 0;
-  unsigned int subroute = 0;
+  unsigned int route = 1;
+  unsigned int subroute = 3;
   boolean active = false;
   String routeTitle = "";
   String subRouteTitle = "";
@@ -77,39 +77,115 @@ byte eventEmmiter() {
   return 0;
 }
 
+
+void enableSensorTest() {
+  digitalWrite(LASER_S, HIGH);
+  lcd.setCursor(0, 1);
+  lcd.print("                                      ");
+  lcd.setCursor(0, 1);
+  lcd.print("Started");
+  lcd.setCursor(10, 1);
+  lcd.print("V0:");
+  lcd.setCursor(10, 2);
+  lcd.print("V1:");
+  lcd.setCursor(10, 3);
+  lcd.print("VA:");
+  lcd.setCursor(0, 3);
+  lcd.print("dV:");
+  lcd.setCursor(0, 2);
+  lcd.print("V:");
+}
+
+void disableSensorTest() {
+  digitalWrite(LASER_S, LOW);
+  lcd.setCursor(0, 1);
+  lcd.print("                                      ");
+  lcd.setCursor(0, 1);
+  lcd.print("Stopped");
+}
+
+unsigned long lastLaserSwitchTime, LaserSwitchPeriod=500;
+boolean LaserState=true;
+unsigned int laser0=1, laser1=2, laserA=3;
+void SensorTest() {
+  if(millis() - lastLaserSwitchTime > LaserSwitchPeriod) {
+
+    if(LaserState) {
+      laser1 = analogRead(LASER_SENS_S);
+      lcd.setCursor(14, 2);
+      lcd.print("     ");
+      lcd.setCursor(14, 2);
+      lcd.print(laser1);
+    } else {
+      laser0 = analogRead(LASER_SENS_S);
+      lcd.setCursor(14, 1);
+      lcd.print("     ");
+      lcd.setCursor(14, 1);
+      lcd.print(laser0);
+    }
+    laserA = (laser0+laser1)/2;
+    lcd.setCursor(14, 3);
+    lcd.print("     ");
+    lcd.setCursor(14, 3);
+    lcd.print(laserA);
+
+    lcd.setCursor(4, 3);
+    lcd.print("   ");
+    lcd.setCursor(4, 3);
+    lcd.print((laser1-laser0));
+
+    lcd.setCursor(4, 2);
+    lcd.print("     ");
+    lcd.setCursor(4, 2);
+    lcd.print(analogRead(LASER_SENS_S));
+
+    LaserState=!LaserState;
+    digitalWrite(LASER_S, LaserState);
+    lastLaserSwitchTime = millis();
+  }
+}
+
+void LaserSwitchPeriodDec() {
+  if(LaserSwitchPeriod<=2) return;
+  LaserSwitchPeriod = LaserSwitchPeriod/2;
+  lcd.setCursor(0, 1);
+  lcd.print("          ");
+  lcd.setCursor(0, 1);
+  lcd.print("T:");
+  lcd.print(LaserSwitchPeriod);
+}
+
+void LaserSwitchPeriodInc() {
+  if(LaserSwitchPeriod>=10000) return;
+  LaserSwitchPeriod = LaserSwitchPeriod*2;
+  lcd.setCursor(0, 1);
+  lcd.print("          ");
+  lcd.setCursor(0, 1);
+  lcd.print("T:");
+  lcd.print(LaserSwitchPeriod);
+}
+
 boolean eventListener(byte eventCode) {
 
   if(state.route==1) {
     if(state.subroute==3) {
-      if(!state.active) {
+      if(state.active) {
         if(eventCode==12) {
-          digitalWrite(LASER_S, HIGH);
-          lcd.setCursor(0, 2);
-          lcd.print("                           ");
-          lcd.setCursor(0, 2);
-          lcd.print("LASER ON");
-          state.active=!state.active;
-          eventCode=0;
-        }
-      } else {
-        if(eventCode==12) {
-          digitalWrite(LASER_S, LOW);
-          lcd.setCursor(0, 2);
-          lcd.print("                           ");
-          lcd.setCursor(0, 2);
-          lcd.print("LASER OFF");
+          disableSensorTest();
           state.active=!state.active;
           return true;
         }
-        if(millis() - lastDispRefreshTime > 1000) {
-          lcd.setCursor(12, 2);
-          lcd.print("                           ");
-          lcd.setCursor(12, 2);
-          lcd.print(analogRead(LASER_SENS_S));
-          lastDispRefreshTime = millis();
+        if(eventCode==10) {LaserSwitchPeriodDec(); eventCode=0;}
+        if(eventCode==11) {LaserSwitchPeriodInc(); eventCode=0;}
+        SensorTest();
+        return true;
+      } else {
+        if(eventCode==12) {
+          enableSensorTest();
+          state.active=!state.active;
+          eventCode=0;
         }
 
-        return true;
       }
     }
   }
@@ -178,10 +254,20 @@ void menuNext() {
   State newState = state;
   if(newState.subroute) {
     newState.subroute++;
+    newState.active = false;
   } else {
     newState.route++;
+    newState.active = false;
   }
   state = menu(newState,  state);
+  lcd.setCursor(0, 1);
+  lcd.print("                          ");
+  lcd.setCursor(0, 2);
+  lcd.print("                          ");
+  lcd.setCursor(0, 3);
+  lcd.print("                          ");
+  lcd.setCursor(0, 4);
+  lcd.print("                          ");
 }
 
 void menuPrev() {
@@ -189,11 +275,19 @@ void menuPrev() {
   if(newState.subroute) {
     if(newState.subroute>1) {
       newState.subroute--;
+      newState.active = false;
     }
   } else {
     newState.route--;
+    newState.active = false;
   }
   state = menu(newState,  state);
+  lcd.setCursor(0, 2);
+  lcd.print("                          ");
+  lcd.setCursor(0, 3);
+  lcd.print("                          ");
+  lcd.setCursor(0, 4);
+  lcd.print("                          ");
 }
 
 void menuEnter() {
